1362edd0a39dfe259f53e55156df825f35231152
GuidoMartinez1 <guido.martinez20@gmail.com>
2020-05-31 22:26:36 -0300

    Punto 1 y 2 Terminado (con test, con mini consultita)

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,51 @@
 module Lib where
 import Text.Show.Functions
 
-laVerdad = True
+data Gimnasta = Gimnasta {
+    nombre :: String,
+    edad :: Float,
+    peso :: Float,
+    coefTonificacion :: Float
+} deriving (Show, Eq)
+
+pancho :: Gimnasta
+pancho = Gimnasta "Francisco" 40.0 120.0 1.0
+andres :: Gimnasta
+andres = Gimnasta "Andy" 22.0 80.0 6.0 
+
+type Ejercicio = CantMinutos -> Gimnasta -> Gimnasta
+
+type CantMinutos = Float
+
+relax :: Ejercicio
+relax minutos gimnasta = gimnasta
+
+--PUNTO 1 
+saludable :: Gimnasta -> Bool
+saludable gimnasta = (not (esObeso gimnasta)) && (coefTonificacion gimnasta > 5)
+
+esObeso :: Gimnasta -> Bool
+esObeso = (>100).peso
+
+noEsObeso :: Gimnasta ->Bool
+noEsObeso = not.esObeso
+
+--PUNTO 2
+type CaloriasAQuemar = Float
+
+quemarCalorias :: Gimnasta -> CaloriasAQuemar -> Gimnasta
+quemarCalorias gimnasta calorias 
+    | esObeso gimnasta = disminuirCalorias (caloriasObeso calorias) gimnasta
+    | (noEsObeso gimnasta) && ((>30) (edad gimnasta)) && (calorias > 200) = disminuirCalorias 1 gimnasta
+    | otherwise =disminuirCalorias (cuentaRara calorias gimnasta) gimnasta
+
+cuentaRara :: Float -> Gimnasta -> Float
+cuentaRara calorias (Gimnasta _ edad peso _) = calorias / (edad * peso)
+
+disminuirCalorias :: Float -> Gimnasta -> Gimnasta
+disminuirCalorias cantidad gimnasta  = gimnasta {peso = peso gimnasta - (cantidad * 1.0) }
+
+caloriasObeso :: Float -> Float
+caloriasObeso cantidad =  cantidad / 150
+
+--PUNTO 3 
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,15 @@ import Lib
 
 main :: IO()
 main = hspec $ do
-   describe "Tests de prueba" $ do
-      it "la verdad y la verdad" $ do
-         laVerdad `shouldBe` True
+   describe "Ejercicio Gimnasta" $ do
+      it "pancho luego de hacer relax" $ do
+         relax 10 pancho `shouldBe` pancho
+      it "pancho es saludable?" $ do
+         saludable pancho `shouldBe` False
+      it "andres es saludable?" $ do
+         saludable andres `shouldBe` True
+      it "quemarCalorias a pancho 300" $ do
+         quemarCalorias pancho 300 `shouldBe` (Gimnasta "Francisco" 40.0 118.0 1.0)
+      it "quemarCalorias a andres 300" $ do
+         quemarCalorias andres 300 `shouldBe` (Gimnasta "Andy" 22.0 79.829544 6.0) --SI NO LE AGREGO TODOS ESOS DECIMALES ROMPE
+     
